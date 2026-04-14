6d107b5bd4fe2608fa6abe5d8143005cf5b890cd
Rizky Dhani <rizkydhni03@gmail.com>
2026-04-14 13:22:15 +0700

    feat: add navigation translation files for EN and ID locales
    - Create navigation.php with Devices, Admin Management, User Management translations
    - English: Devices, Admin Management, User Management
    - Indonesian: Perangkat, Manajemen Admin, Manajemen Pengguna

---
+++ resources/lang/en/navigation.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'Devices' => 'Devices',
+    'Admin Management' => 'Admin Management',
+    'User Management' => 'User Management',
+];
---
+++ resources/lang/id/navigation.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'Devices' => 'Perangkat',
+    'Admin Management' => 'Manajemen Admin',
+    'User Management' => 'Manajemen Pengguna',
+];
